c46e413eb1591bc75356cd50aa98404fbbea4db7
nFd_noFugaziDev <mubaraqmuhammad06@gmail.com>
2025-04-23 22:55:46 +0100

    dep: added nativewind css for styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
+import React from "react";
 import { Text, View } from "react-native";
+import './global.css'
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-3xl text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
   );
 }
